4e239276c2f5feb3bdbad34628f5d8169b967851
Sienna <remicknsisi@gmail.com>
2022-09-28 09:55:07 -0400

    commit tree 38622ebb2dcc82b36c236fe1bc0e2c9369e9cc56

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = function(){
+  let makeTutorialArrays = tutorials.map(function(tutorial){
+    return tutorial.split(' ').map(function(word){
+      return word[0].toUpperCase()+ word.slice(1)
+    }).join(' ')
+});
+return makeTutorialArrays
 }
